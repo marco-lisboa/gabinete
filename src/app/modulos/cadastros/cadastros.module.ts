2e3f0ffc82d89d427fc8f8a99b03112a133c88c9
marco-lisboa <93157451+marco-lisboa@users.noreply.github.com>
2022-03-22 18:38:41 -0300

    criacão do cadastro de assunto

--- src/app/modulos/cadastros/cadastros.module.ts
+++ src/app/modulos/cadastros/cadastros.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerCadastrosComponent } from './container/container-cadastros/container-cadastros.component';
-import { CardMenuComponent } from './components/card-menu/card-menu.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CadastrosRoutingModule } from './cadastros-routing.module';
 import { BoxComponent } from './components/box/box.component';
@@ -14,7 +13,6 @@ import { ListarAssuntosComponent } from './components/assuntos/listar-assuntos/l
 @NgModule({
   declarations: [
     ContainerCadastrosComponent,
-    CardMenuComponent,
     BoxComponent,
     ListarAssuntosComponent
   ],
